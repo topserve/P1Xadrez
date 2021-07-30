6fcdf684ba392bf6cf4dd3b326e9b7d02a0c8170
kleber <kleber.xhellx@gmail.com>
2021-07-30 12:44:23 -0300

    inicio do xadrez

---
+++ P1Xadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P1Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
