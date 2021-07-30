4a93cbbe42fedf711ff6438de70a755d003f9b53
kleber <kleber.xhellx@gmail.com>
2021-07-30 13:54:48 -0300

    Classe Posicao

--- P1Xadrez/Program.cs
+++ P1Xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace P1Xadrez
 {
@@ -6,6 +7,10 @@ namespace P1Xadrez
     {
         static void Main(string[] args)
         {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
         }
     }
 }
