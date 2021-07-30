d046520c39bdbdb2db1b567fb1276821ff501fb5
kleber <kleber.xhellx@gmail.com>
2021-07-30 17:17:20 -0300

    posicao invertida

--- P1Xadrez/Program.cs
+++ P1Xadrez/Program.cs
@@ -8,22 +8,9 @@ namespace P1Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-
-
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('g',8 );
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
 
             Console.ReadLine();
         }
