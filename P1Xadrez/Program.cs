a572233e4deeec966ebd0857fb20b0db6d4708a1
kleber <kleber.xhellx@gmail.com>
2021-07-30 14:38:12 -0300

    montrando tabuleiro na tela

--- P1Xadrez/Program.cs
+++ P1Xadrez/Program.cs
@@ -8,6 +8,9 @@ namespace P1Xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
