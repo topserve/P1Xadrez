7724c929eaefe0276feef623a7b429fcc59e9b91
kleber <kleber.xhellx@gmail.com>
2021-07-30 14:24:14 -0300

    Inicio do Tabuleiro

--- P1Xadrez/Program.cs
+++ P1Xadrez/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace P1Xadrez
 {
@@ -7,9 +7,7 @@ namespace P1Xadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
         }
     }
